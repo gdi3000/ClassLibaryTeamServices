e779f6d563157d33f5d855ccf7b1bc3c998d0f8c
gdi3000 <dani056i@edu.easj.dk>
2018-03-05 13:27:43 +0100

    Now With UnitTests

---
+++ ClassLibaryTeamServicesTests/PersonTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibaryTeamServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibaryTeamServices.Tests
+{
+    [TestClass()]
+    public class PersonTests
+    {
+        [TestMethod()]
+        public void SuccessfulGetNameTest()
+        {
+            Person P = new Person();
+            P.GetName();
+
+            Assert.AreSame(P.GetName(),"Daniel Hahnemann");
+        }
+
+        [TestMethod()]
+        public void UnsuccessfulGetNameTest()
+        {
+            Person P = new Person();
+            P.GetName();
+
+            Assert.AreNotEqual(P.GetName(), "Bob");
+        }
+
+        [TestMethod()]
+        public void SuccessfulGetAddressTest()
+        {
+            Person P = new Person();
+            P.GetAddress();
+
+            Assert.AreSame(P.GetAddress(), "Elisagaardsvej 5");
+        }
+
+        public void UnsuccessfulGetAddressTest()
+        {
+            Person P = new Person();
+            P.GetAddress();
+
+            Assert.AreNotEqual(P.GetAddress(), "");
+        }
+    }
+}
---
+++ UnitTests/PersonUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PersonUnitTest
+    {
+        [TestMethod] 
+        public string GetNameTest()
+        {
+         
+         Person P = new Person();
+         P.GetName;
+        }
+    }
+}
