4af427e0fe5c86488bfb6f51915036f7d8b4567f
gdi3000 <dani056i@edu.easj.dk>
2018-03-05 13:45:10 +0100

    vdsdfs

---
+++ WcfServiceSOAPTeamServicesTests/Service1Tests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfServiceSOAPTeamServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceSOAPTeamServices.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetAddressSoapTest()
+        {
+            
+            
+                Service1 P = new Service1();
+                
+
+                Assert.AreSame(P.GetAddressSoap(), "Addressen");
+            
+        }
+
+        [TestMethod()]
+        public void GetNameSoapTest()
+        {
+            Service1 P = new Service1();
+            Assert.AreSame(P.GetNameSoap(), "Daniel Hahnemann");
+        }
+    }
+}
